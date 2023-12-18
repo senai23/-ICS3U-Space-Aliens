ae1a5cd51f2e499d02910d4a1e9f1109273a0168
Senai Mehari <senai.mehari@stu.ocsb.ca>
2023-12-18 12:34:48 -0500

    code.py

---
+++ code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Created by: Senai
+# Created on: December 2023
+# This program is the "Space Aliens" program on the PyBadge
+
+import ugame
+import stage
+
+
+def game_scene():
+    # this function is the main game game_scene
+
+    print("\n\n\n") # 3 blank lines
+    print("Hello, Senai!")
+
+    #repeat forever. game loop
+    while True":
+        pass # just a placeholder for now
+
+if __name_- == "__main__":
+    game_scene()
