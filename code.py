c1f0c2cb9ce25893c31347fd323dc34374c24948
Senai Mehari <senai.mehari@stu.ocsb.ca>
2023-12-21 12:08:33 -0500

    constants.py

--- code.py
+++ code.py
@@ -1,22 +1,82 @@
 #!/usr/bin/env python3
 
+
 # Created by: Senai
 # Created on: December 2023
 # This program is the "Space Aliens" program on the PyBadge
 
+
 import ugame
 import stage
-
+import constants
 
 def game_scene():
     # this function is the main game game_scene
 
-    print("\n\n\n") # 3 blank lines
-    print("Hello, Senai!")
+    # image banks for CircuitPython
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
-    #repeat forever. game loop
-    while True":
-        pass # just a placeholder for now
 
-if __name_- == "__main__":
+    # set the background to image 0 in the image bank
+    #   and the size (10x8 tiles of size 16x16)
+    background = stage.Grid(image_bank_background, 10, 8)
+
+
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+
+
+    # create a stage for the background to show up on
+    #   and set the frame rate to 60fps
+    game= stage.Stage(ugame.display, 60)
+    # set the layers, items show up in order
+    game.layers = [ship] + [background]
+    # render the background and the initial location of sprite list
+    # most likey you will only render the background once per game scene
+    game.render_block()
+   
+   
+    # repeat forever, game loop
+    while True:
+        # get user input
+        keys = ugame.buttons.get_pressed()
+
+
+        if keys & ugame.K_X:
+            print("pass")
+        if keys & ugame.K_O:
+            print("pass")
+        if keys & ugame.K_START:
+            print("pass")
+        if keys & ugame.K_SELECT:
+            print("pass")
+        if keys & ugame.K_RIGHT:
+            if ship.x <= constants.SCREEN_X - constants.SPRITE_SIZE:
+                ship.move(ship.x + 1, ship.y)
+        else:
+            ship.move(constants.SCREEN_X - constants.SPRITE_SIZE, ship.y)
+
+
+        if keys & ugame.K_LEFT:
+            if ship.x >= 0:
+                ship.move(ship.x - 1, ship.y)
+            else:
+                ship.move(0, ship.y)
+        if keys & ugame.K_UP:
+            ship.move(ship.x, ship.y - 1)
+        if keys & ugame.K_DOWN:
+            ship.move(ship.x, ship.y + 1)
+
+
+
+
+        # update game logic
+
+
+        # redraw Sprites
+        game.render_sprites({ship})
+        game.tick()
+
+
+if __name__ == "__main__":
     game_scene()
---
+++ constants.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by: Senai
+# Created on: December 2023
+# This constants file is for Space Alien game
+
+
+# Pybadge screen size is 160x128 and sprites are 16x16
+SCREEN_X= 160
+SCREEN_Y= 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
