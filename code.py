2a925dc45a6c7952087356739c56a75ef390c0e6
Senai Mehari <senai.mehari@stu.ocsb.ca>
2024-01-08 13:10:29 -0500

    changed code.py

--- code.py
+++ code.py
@@ -6,9 +6,52 @@
 # This program is the "Space Aliens" program on the PyBadge
 
 
+# Pybadge screen size is 160x128 and sprites are 16x16
 import ugame
 import stage
 import constants
+
+def menu_scene():
+    # this function is the main game game_scene
+
+    # image banks for CircuitPython
+    image_bank_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+    
+    # add text objects
+    text=[]
+    text1 = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALLETTE, buffer=None)
+    text1.move(20, 10)
+    text1. text("MT Game Studios")
+    text.append(text1)
+
+    text2 = stage.Text(width=29, height=12, font=None, palette= constants.RED_PALLETTE, buffer=None)
+    text2.move(40, 110)
+    text2. text("PRESS START")
+    text.append(text2)
+
+    # set the background to image 0 in the image bank
+    #   and the size (10x8 tiles of size 16x16)
+    background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y) 
+ 
+    # create a stage for the background to show up on
+    #   and set the frame rate to 60fps
+    game= stage.Stage(ugame.display, 60)
+    # set the layers, items show up in order
+    game.layers = text + [background]
+    # render the background and the initial location of sprite list
+    # most likey you will only render the background once per game scene
+    game.render_block()
+   
+    # repeat forever, game loop
+    while True:
+        # get user input
+        keys = ugame.buttons.get_pressed()
+
+        if keys & ugame.K_START != 0:
+            game_scene()
+    
+        # redraw Sprites
+        game.tick()
 
 def game_scene():
     # this function is the main game game_scene
@@ -21,10 +64,10 @@ def game_scene():
     a_button = constants.button_state["button_up"]
     b_button = constants.button_state["button_up"]
     start_button = constants.button_state["button_up"]
-    select_button = constants.button-state["button_up"]
+    select_button = constants.button_state["button_up"]
 
     # get sound ready
-    pew_sound = open("pew.wav", 'rb')\
+    pew_sound = open("pew.wav", 'rb')
     sound = ugame.audio
     sound.stop()
     sound.mute(False)
@@ -35,6 +78,9 @@ def game_scene():
  
     ship = stage.Sprite(image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE))
 
+    alien = stage.Sprite(image_bank_sprites, 9,
+                      int(constants.SCREEN_X / 2 - constants.SPRITE_SIZE / 2),
+                      16)
     # create a stage for the background to show up on
     #   and set the frame rate to 60fps
     game= stage.Stage(ugame.display, 60)
@@ -49,11 +95,21 @@ def game_scene():
         # get user input
         keys = ugame.buttons.get_pressed()
 
-
         if keys & ugame.K_X:
             pass
-        if keys & ugame.K_O:
-            pass
+        # A button to fire
+        if keys & ugame.K_O != 0:
+            if a_button == constants.button_state["button_up"]:
+                a_button = constants.button_state["button_just_pressed"]
+            elif a_button == constants.button_state["button_just_pressed"]:
+                a_button = constants.button_state["button_still_pressed"]
+        else:
+            if a_button == constants.button_state["button_still_pressed"]:
+                a_button = constants.button_state["button_released"]
+            else:
+                a_button = constants.button_state["button_up"]
+        
+            
         if keys & ugame.K_START:
             pass
         if keys & ugame.K_SELECT:
@@ -73,16 +129,15 @@ def game_scene():
         if keys & ugame.K_DOWN:
             pass
 
-
-
-
         # update game logic
-
-
+        # play sound if A was just button_just_pressed
+        if a_button == constants.button_state["button_just_pressed"]:
+            sound.play(pew_sound)
+            pass 
         # redraw Sprites
-        game.render_sprites({ship})
+        game.render_sprites([ship] + [alien])
         game.tick()
 
 
 if __name__ == "__main__":
-    game_scene()
+    menu_scene()
--- constants.py
+++ constants.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-
-# Created by: Senai
-# Created on: December 2023
-# This constants file is for Space Alien game
-
-
-# Pybadge screen size is 160x128 and sprites are 16x16
 SCREEN_X = 160
 SCREEN_Y = 128
 SCREEN_GRID_X = 10
@@ -15,10 +7,13 @@ TOTAL_NUMBER_OF_ALIENS = 5
 FPS = 60
 SPRITE_MOVEMENT_SPEED = 1
 
-# Using for button stat
+# Using for button state
 button_state = {
-    "button_up" : "up"
+    "button_up": "up",
     "button_just_pressed": "just pressed",
     "button_still_pressed": "still pressed",
     "button_released": "released"
 }
+# new pallet for red filled text
+RED_PALLETTE = (b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
